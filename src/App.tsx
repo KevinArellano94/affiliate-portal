f723669716498abd5718bb186fc01bd214e7cc71
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2025-02-09 07:55:18 -0500

    Remove unused prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Router>
