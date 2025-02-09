a636db724e86116facc1082eaaf9c6f8308a4fd3
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2025-02-09 17:39:48 -0500

    Added registration logic, and handling.

--- src/utils/authentication/registerUser.ts
+++ src/utils/authentication/registerUser.ts
@@ -1,11 +1,8 @@
 
 
-const GRAPHQL_URL = `https://apollo-render-bd2p.onrender.com`;
-const GRAPHQL_TOKEN = `derp`;
-
 export const registerUser = async (email: string, password: string, fullName: string): Promise<boolean> => {
-    // const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL as string;
-    // const GRAPHQL_TOKEN = import.meta.env.VITE_GRAPHQL_TOKEN as string;
+    const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL as string;
+    const GRAPHQL_TOKEN = import.meta.env.VITE_GRAPHQL_TOKEN as string;
 
     const query = `mutation AddUser($input: UserInput!) {
         addUser(input: $input) {
--- src/utils/function/userGenerateUrl.ts
+++ src/utils/function/userGenerateUrl.ts
@@ -1,9 +1,7 @@
 
 
-const TARGET_URL = 'http://localhost:3000';
-
 export const generateAffiliateUrl = (link: any) => {
-    // const TARGET_URL = import.meta.env.VITE_TARGET_URL as string;
+    const TARGET_URL = import.meta.env.VITE_TARGET_URL as string;
 
     const baseUrl = `${ TARGET_URL }/affiliate`;
     const params = new URLSearchParams({
