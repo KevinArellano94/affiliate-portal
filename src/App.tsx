8f1532d7778f8da705fcb1584d267137ba629a04
Kevin Arellano <Kevin.Arellano94@GMail.Com>
2025-02-13 08:54:44 -0500

    Added new template dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,29 @@ import { useState, Suspense, lazy } from "react";
 import './App.css'
 import Home from './pages/Home'
 import Register from './pages/Register'
-import Login from './pages/Login'
+// import Login from './pages/Login'
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 
 
 function App() {
-	const [user, setUser] = useState(null);
-	console.log(user);
+	// const [user, setUser] = useState(null);
+
+	const user: any = {
+		"id": "481bdbbd-e2be-41a7-939d-8f80698dfaf0",
+		"email": "gabriel@gmail.com",
+		"hashedPassword": "$2b$12$.ulPAmw2UFBr8eUgRogHy.9DI7tInZ3h2jmY6Kn339mXwbujf92IO",
+		"fullName": "gabriel",
+		"contactInfo": {
+		"cell": "404-404-4004"
+		},
+		"taxInfo": {},
+		"bankingInfo": {},
+		"twoFactorSettings": {},
+		"createdAt": "2025-02-08T05:00:15.867Z",
+		"updatedAt": "2025-02-09T18:18:09.726Z",
+		"isActive": true,
+		"token": "eyJ...Jye"
+	}
 
 	return (
 		<>
@@ -17,10 +33,8 @@ function App() {
 			<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/register" element={<Register />} />
-			<Route path="/login" element={<Login setUser={ setUser } />} />
-			{/* <Route path="/dashboard" element={<Suspense fallback={<div>Loading...</div>}><Dashboard user={ user } /></Suspense>} /> */}
-			<Route path="/dashboard" element={<Suspense fallback={<div>Loading...</div>}><Dashboard /></Suspense>} />
-			{/* <Route path="/dashboard" element={<Dashboard />}/> */}
+			{/* <Route path="/login" element={<Login setUser={ setUser } />} /> */}
+			<Route path="/dashboard" element={<Suspense fallback={<div>Loading...</div>}><Dashboard user={ user } /></Suspense>} />
 			</Routes>
 		</Router>
 		</>
